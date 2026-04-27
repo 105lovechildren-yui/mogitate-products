cf4b5152088d9d9737d07067752729e5556fb4bb
yui hito <105.love.children@gmail.com>
2026-04-27 11:20:43 +0900

    ルーティング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//商品一覧ページ
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+//商品登録ページ
+Route::get('/products/register', [ProductController::class, 'register'])->name('products.register');
+Route::post('/products/register', [ProductController::class, 'store'])->name('products.store');
+
+//商品詳細ページ
+Route::get('/products/detail/{productId}', [ProductController::class, 'show'])->name('products.show');
+
+//商品更新
+Route::post('/products/{productId}/update', [ProductController::class, 'update'])->name('products.update');
+
+//商品検索
+Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
+
+//削除
+Route::delete('/products/{productId}/delete', [ProductController::class, 'destroy'])->name('products.destroy');
